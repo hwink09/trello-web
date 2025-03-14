a0cf3fc62b23c4437f88559243301abf1ed5a66d
Cao Tran Hoang Minh <caotranhoangminh2003@gmail.com>
2025-03-14 21:56:26 +0700

    Add CSS Baseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import CssBaseline from "@mui/material/CssBaseline";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
