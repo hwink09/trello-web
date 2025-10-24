60831b4450b4bd50f4a190caa62e0b38c2214eab
Cao Trần  Hoàng Minh <caotranhoangminh2003@gmail.com>
2025-10-24 23:34:29 +0700

    quick fix

--- src/pages/Settings/AccountTab.jsx
+++ src/pages/Settings/AccountTab.jsx
@@ -106,7 +106,7 @@ function AccountTab() {
           <Box>
             <Avatar
               sx={{ width: 84, height: 84, mb: 1 }}
-              alt="TrungQuanDev"
+              alt="HwwinkDev"
               src={currentUser?.avatar}
             />
             <Tooltip title="Upload a new image to update your avatar immediately.">
