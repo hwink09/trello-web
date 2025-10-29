7add704fdeee4cdd56b0a22f9eb1e3aa3b696307
Cao Trần  Hoàng Minh <caotranhoangminh2003@gmail.com>
2025-10-29 09:15:32 +0700

    fix: update production API root URL to correct endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ if (process.env.BUILD_MODE === 'dev') {
 }
 
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-txk4.onrender.com'
+  apiRoot = 'https://trello-api-63zq.onrender.com'
 }
 export const API_ROOT = apiRoot
 
